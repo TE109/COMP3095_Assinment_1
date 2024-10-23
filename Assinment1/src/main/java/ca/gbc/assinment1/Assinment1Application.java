ff0faf1d660d5f380106fc1988818fd314f4a422
TE109 <1991tmce@gmail.com>
2024-10-23 09:47:51 -0400

    Assignment 1 commit

---
+++ Assinment1/src/main/java/ca/gbc/assinment1/Assinment1Application.java
@@ -0,0 +1,13 @@
+package ca.gbc.assinment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assinment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assinment1Application.class, args);
+    }
+
+}
