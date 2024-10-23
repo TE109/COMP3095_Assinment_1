b0367a2cc1326b2adf0d530d3a17de56e3999807
TE109 <1991tmce@gmail.com>
2024-10-23 09:47:51 -0400

    Assignment 1 commit

---
+++ Assinment1/src/test/java/ca/gbc/assinment1/Assinment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.assinment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assinment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
